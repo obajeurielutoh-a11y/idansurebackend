ff18a86e0ced0f0694dee595370a83a967cc3335
obajeurielutoh-a11y <obajeurielutoh@gmail.com>
2025-11-25 12:44:26 +0100

    fixed cors issues

--- SubscriptionSystem/Program.cs
+++ SubscriptionSystem/Program.cs
@@ -234,7 +234,7 @@ builder.Services.AddCors(options =>
                                "https://idansure.com",
                                "https://www.idansure.com",
                                "https://idansure.vercel.app",
-                               "https://localhost:7221") // Allow only these origins
+                               "https://localhost:7221", "api.idansure.com") // Allow only these origins
                                
                   .AllowAnyHeader()
                   .AllowAnyMethod()
